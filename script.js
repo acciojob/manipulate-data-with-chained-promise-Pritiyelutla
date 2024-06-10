2d3cf09899c4704149d414a9731311130d6d96b1
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-10 23:40:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+
+let div = document.getElementById("output");
+const arr = [1, 2, 3, 4];
+
+function promOdd(arr)
+{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let oddArr = [];
+            for(let i=0; i<arr.length; i++)
+            {
+                if(arr[i] % 2 == 0)
+                {
+                    oddArr.push(arr[i]);
+                }
+            }
+            resolve(oddArr);
+        }, 1000);
+    });
+}
+
+function promEven(arr)
+{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let res =[];
+            for(let i=0; i<arr.length; i++)
+            {
+                if(arr[i] % 2 == 0)
+                {
+                    res.push((arr[i])*2);
+                }
+            } 
+            resolve(res);
+        }, 2000);
+    });
+}
+
+
+promOdd(arr)
+.then((res) => {
+    div.innerText = res;
+    //console.log("back in chain");//ye line nhi execute ho rha i am not coming here only
+    promEven(arr)
+    .then((res) => {
+        div.innerText = res;
+    });
+});
